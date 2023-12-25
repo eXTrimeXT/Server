7a6f608d27b14d9e6c6ca2f2e8240857fc657abf
troxXxyl <fhxuggvkxh6@gmail.com>
2023-12-25 16:37:33 +0300

    Прикол вот тут: \`// Контроллеры для админ панели router.get('/', adminController.getArticleAll); router.get('/:idArticle', adminController.getArticle); router.post('/addArticle', adminController.addArticle); router.post('/editArticle', adminController.editArticle); router.post('/deleteArticle', adminController.deleteArticle); \` > Обратите внимание на `:`

--- routes/admin.js
+++ routes/admin.js
@@ -8,15 +8,9 @@ const jsonParser = express.json();
 // Контроллеры для админ панели
 router.get('/', adminController.getArticleAll);
 router.get('/:idArticle', adminController.getArticle);
-<<<<<<< HEAD
 router.post('/addArticle', adminController.addArticle);
 router.post('/editArticle', adminController.editArticle);
 router.post('/deleteArticle', adminController.deleteArticle);
-=======
-router.post('/:addArticle', adminController.addArticle);
-router.post('/:editArticle', adminController.editArticle);
-router.post('/:deleteArticle', adminController.deleteArticle);
->>>>>>> ee128d6845d19a6d62f785a45ffb4d3a37aaec28
 
 
 console.log('route/admin');
