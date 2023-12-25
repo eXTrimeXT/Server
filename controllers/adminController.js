465766d61528319d334e9d258083c7d58a165b38
eXTrimeXT <timurmalishevxt57@gmail.com>
2023-12-25 21:43:13 +0300

    add /public/css/1.css

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,31 +1,31 @@
 const Models = require("../models/adminModel.js");
 
-exports.getArticleAll = async function getA2(req, res){
+exports.getArticleAll = async function(req, res){
     let m = await Models.getAll();
     res.render('adminArticles', {data:m});
 };
 
-exports.getArticle = async function get2(req, res){
+exports.getArticle = async function(req, res){
     let m = await Models.getOne(req.params.idArticle);
     console.log("adminController: getArticle");
     res.render('adminArticleShow', {data:m});
 };
 
-exports.addArticle = async function add2(req, res){
+exports.addArticle = async function(req, res){
     await Models.addOne(req.body);
     let m = await Models.getAll();
     console.log("adminController: addArticle");
     res.render('adminArticles', {data:m});
 };
 
-exports.editArticle = async function edit2(req, res){
+exports.editArticle = async function(req, res){
     await Models.editOne(req.body);
     let m = await Models.getOne(req.body.idArticle);
     console.log("adminController: editArticle");
     res.render('adminArticleShow', {data:m});
 };
 
-exports.deleteArticle = async function del2(req, res){
+exports.deleteArticle = async function(req, res){
     await Models.deleteOne(req.body.idArticle);
     let m = await Models.getAll();
     console.log("adminController deleteArticle");
--- models/adminModel.js
+++ models/adminModel.js
@@ -3,7 +3,7 @@ const connection = require("../mysql.js");
 const { response } = require("express");
 
 
-exports.getAll = async function getA(req, res){
+exports.getAll = async function(req, res){
     let arr = [];
     console.log("adminModel getAll");
     await connection.query("SELECT * FROM article")
@@ -19,7 +19,7 @@ exports.getAll = async function getA(req, res){
     return arr;
 }
 
-exports.getOne = async function get(req, res){
+exports.getOne = async function(req, res){
     let arr = [];
     let sql = "SELECT * FROM article WHERE idArticle=?";
     filter = [req];
@@ -36,7 +36,7 @@ exports.getOne = async function get(req, res){
     return arr;
 }
 
-exports.addOne = async function add(req, res){
+exports.addOne = async function(req, res){
     let sql = "INSERT INTO article VALUES ('', ?, ?, ?)";
     console.log("adminModel addOne: " + req.idArticle + " : "+ req.titleArticle + " : " + req.textArticle + " : " + req.description);
     filter = [req.titleArticle, req.textArticle, req.description];
@@ -49,7 +49,7 @@ exports.addOne = async function add(req, res){
     });
 }
 
-exports.editOne = async function edit(req, res){
+exports.editOne = async function(req, res){
     let arr = [];
     let sql = "UPDATE article SET titleArticle=?, textArticle=?, descriptionArticle=? WHERE idArticle=?";
     filter = [req.titleArticle, req.textArticle, req.description, req.idArticle];
@@ -67,7 +67,7 @@ exports.editOne = async function edit(req, res){
     return arr;
 }
 
-exports.deleteOne = async function del(req, res){
+exports.deleteOne = async function(req, res){
     console.log("adminModel deleteOne");
     console.log("adminModel: req = " + req);
     // let sql = "DELETE FROM article WHERE idArticle=?";
