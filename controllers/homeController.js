62b6239d68a31e156d8771b899c21f5c4f327d64
troxXxyl <fhxuggvkxh6@gmail.com>
2023-12-26 10:04:33 +0300

    Актуальная версия

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -2,7 +2,7 @@ exports.getIndex = function(request, response){
     response.send(
         "<p><a href='http://localhost:3000/admin/articles'>Admin Articles</a></p>" + 
         "<p><a href='http://localhost:3000/admin/news'>Admin News</a></p>" + 
-        "<p><a href='http://localhost:3000/articles'>Article</a></p> " + 
+        "<p><a href='http://localhost:3000/article'>Article</a></p> " + 
         "<p><a href='http://localhost:3000/news'>News</a></p> "
     );
 };
--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 let express = require('express');
 var app = express();
+app.disable('view cache');
 var bodyParser = require('body-parser');
 var urlencodedParser = bodyParser.urlencoded({extended : false});
 app.use(express.static(__dirname + '/public')); // Для разрешения MIME типов
 app.set('view engine', 'ejs');
-
 app.use(bodyParser.urlencoded({extended: true})); // Для принятия данных формы
 app.use(bodyParser.json()); 
 
